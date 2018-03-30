78be8a8a0b6f6aa756cb911bfbc9a52806ab706b
Devin Thompson <Devinell7@gmail.com>
2018-03-29 17:12:37 -0700

    Init

---
+++ Assets/_Scripts/SlowSpin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowSpin : MonoBehaviour {
+
+    public float speed = 10f, translateSpeed = 10f, modifier = 100;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.back, speed * Time.deltaTime);
+        transform.Translate(Vector3.forward *  translateSpeed);
+        translateSpeed = translateSpeed * (1 + (modifier + Time.deltaTime)/ 1000);
+	}
+}
