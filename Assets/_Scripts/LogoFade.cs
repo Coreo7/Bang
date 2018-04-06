34d1a851925b7fe6bb5262bb64369198e26a8cd1
Devin Thompson <Devinell7@gmail.com>
2018-04-06 16:18:41 -0700

    testingDive

---
+++ Assets/_Scripts/LogoFade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+
+public class LogoFade : MonoBehaviour {
+
+    CanvasGroup canvas;
+	// Use this for initialization
+	void Start () {
+        canvas = GetComponent<CanvasGroup>();
+        StartCoroutine(fadeCanvas());
+	}
+	
+    IEnumerator fadeCanvas()
+    {
+        print("In Coroutine");
+        yield return new WaitForSecondsRealtime(10);
+        print("Fading Out");
+        canvas.DOFade(0, 2);
+
+        yield return new WaitForSecondsRealtime(600);
+
+        canvas.DOFade(1, 2);
+
+        fadeCanvas();
+    }
+}
---
+++ Assets/_Scripts/ScrollingUV.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingUV : MonoBehaviour {
+
+    public Vector2 uvAnimateRate = new Vector2(1f, 0);
+
+    int materialIndex = 0;
+    string textureName = "_MainTex";
+
+    Vector2 uvOffset = Vector2.zero;
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        uvOffset += (uvAnimateRate * Time.deltaTime);
+        GetComponent<Renderer>().materials[materialIndex].SetTextureOffset(textureName, uvOffset);
+	}
+}
