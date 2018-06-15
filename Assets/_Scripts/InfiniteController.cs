563ade09a97c4a57c1b160e49f616f5be90bf00d
Devin Thompson <Devinell7@gmail.com>
2018-06-14 19:22:58 -0700

    updates

---
+++ Assets/_Scripts/InfiniteController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Colorful;
+
+public class InfiniteController : MonoBehaviour {
+
+    public AnalogTV analogTv;
+    public float Speed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        analogTv.ScanlinesOffset = Time.time * Speed;
+	}
+}
---
+++ Assets/_Scripts/SpinSkybox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinSkybox : MonoBehaviour {
+
+    public float speedMultiplier = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Sets the float value of "_Rotation", adjust it by Time.time and a multiplier.
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speedMultiplier);
+    }
+}
